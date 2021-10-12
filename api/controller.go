746085b65571db35f3bb1de4962d88af03126b58
JObog <jobogbaimhe@gmail.com>
2021-10-12 17:28:20 -0400

    added core functionality for getting a specific states border

--- api/controller.go
+++ api/controller.go
@@ -3,10 +3,18 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func GetStateBorders(w http.ResponseWriter, r *http.Request) {
-	res, _ := json.Marshal(mapper)
-	w.WriteHeader(200)
-	w.Write(res)
+	state := mux.Vars(r)["state"]
+
+	if v, in := mapper[state]; in { //check if key in map
+		res, _ := json.Marshal(v)
+		w.WriteHeader(200)
+		w.Write(res)
+	} else {
+		w.WriteHeader(404)
+	}
 }
--- api/mapper.go
+++ api/mapper.go
@@ -1,5 +1,12 @@
 package api
 
-var mapper = map[string]string{
-	"foo": "bar",
+type Location struct {
+	Name         string   `json:"name"`
+	Abbreviation string   `json:"abbreviation"`
+	Borders      []string `json:"borders"`
+}
+
+var mapper = map[string]Location{
+	"AL": {"Alabama", "AL", []string{"TN", "GA", "MS", "FL"}},
+	"AK": {"Alaska", "AK", []string{}},
 }
