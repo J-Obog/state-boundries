60906f95bd7facc574b481d83dbc5b243f42b2d4
JObog <jobogbaimhe@gmail.com>
2021-10-13 08:15:10 -0400

    added marshal error checking for single state route

--- api/controller.go
+++ api/controller.go
@@ -11,22 +11,29 @@ func GetStateBorders(w http.ResponseWriter, r *http.Request) {
 	state := mux.Vars(r)["state"]
 
 	if v, in := mapper[state]; in { //check if key in map
-		res, _ := json.Marshal(v)
+		res, err:= json.Marshal(v)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		
 		w.WriteHeader(200)
 		w.Write(res)
-	} else {
-		w.WriteHeader(404)
-	}
+		return
+	} 
+
+	w.WriteHeader(404)
 }
 
 func GetAllStateBorders(w http.ResponseWriter, r *http.Request) {
 	res, err := json.Marshal(mapper)
-	
+
 	if err != nil { //check for marshalling error
 		w.WriteHeader(500)
-	} else {
-		w.WriteHeader(200)
-		w.Write(res)
-	}
+		return
+	} 
 
+	w.WriteHeader(200)
+	w.Write(res)
+	return
 }
--- api/mapper.go
+++ api/mapper.go
@@ -9,4 +9,12 @@ type Location struct {
 var mapper = map[string]Location{
 	"AL": {"Alabama", "AL", []string{"TN", "GA", "MS", "FL"}},
 	"AK": {"Alaska", "AK", []string{}},
+	"AZ": {"Arizona", "AZ", []string{"CA", "NV", "UT", "CO", "NM"}},
+	"AR": {"Arkansas", "AR", []string{"MO", "TN", "MS", "LA", "TX", "OK"}},
+	"CA": {"California", "CA", []string{"OR", "NV", "AZ"}},
+	"CO": {"Colorado", "CO", []string{"WY", "NE", "KS", "OK", "NM", "UT"}},
+	"CT": {"Connecticut", "CT", []string{"NY", "MA", "RI"}},
+	"DE": {"Delaware", "DE", []string{"PA", "NJ", "MD"}},
+	"FL": {"Florida", "FL", []string{"GA", "AL"}},
+	"GA": {"Georgia", "GA", []string{"TN", "SC", "FL", "AL"}},
 }
